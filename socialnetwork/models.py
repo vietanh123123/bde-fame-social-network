0ad78d82f5ba9cee2423b24295c1d149ba2ee366
Ngoc Viet Anh Tu <ngtu00001@stud.uni-saarland.de>
2026-06-22 02:58:21 +0200

    T4: Add communities ManyToManyField to SocialNetworkUsers

--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -18,7 +18,9 @@ class SocialNetworkUsers(FameUsers):
     is_banned = models.BooleanField(default=False)
     # T4
     # use this member field to adapt the data model, DO NOT RENAME!
-    communities = None
+    communities = models.ManyToManyField(
+        ExpertiseAreas, related_name="community_members", blank=True
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
