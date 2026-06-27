ddf299cce54280abd2cf6e42a670dbc1d237496b
Ngoc Viet Anh Tu <ngtu00001@stud.uni-saarland.de>
2026-06-27 21:19:25 +0200

    T6: Implement bullshitters HTML view and template

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from socialnetwork.views.html import timeline
 from socialnetwork.views.html import follow
 from socialnetwork.views.html import unfollow
+from socialnetwork.views.html import bullshitters
 from socialnetwork.views.html import toggle_community_mode
 from socialnetwork.views.html import join_community
 from socialnetwork.views.html import leave_community
@@ -15,7 +16,8 @@ urlpatterns = [
     path("html/timeline", timeline, name="timeline"),
     path("api/follow", follow, name="follow"),
     path("api/unfollow", unfollow, name="unfollow"),
-    path("html/toggle_community_mode", toggle_community_mode, name="toggle_community_mode"),
-    path("html/join_community", join_community, name="join_community"),
-    path("html/leave_community", leave_community, name="leave_community"),
+    path("html/bullshitters", bullshitters, name="bullshitters"),
+    path("api/toggle_community_mode", toggle_community_mode, name="toggle_community_mode"),
+    path("api/join_community", join_community, name="join_community"),
+    path("api/leave_community", leave_community, name="leave_community"),
 ]
